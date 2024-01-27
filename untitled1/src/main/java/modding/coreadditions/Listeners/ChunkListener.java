06e54560ed6eb75b43e77a7e06abb009a2312062
ItzFezy <148616668+ItzFezy@users.noreply.github.com>
2024-01-27 15:03:15 -0600

    Update ChunkListener.java

--- untitled1/src/main/java/modding/coreadditions/Listeners/ChunkListener.java
+++ untitled1/src/main/java/modding/coreadditions/Listeners/ChunkListener.java
@@ -1,1 +1,16 @@
+package modding.coreadditions.Listeners;
 
+import io.papermc.paper.event.packet.PlayerChunkLoadEvent;
+import modding.coreadditions.Main;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class ChunkListener implements Listener {
+    private final Main plugin;
+    FileConfiguration config;
+    public ChunkListener(Main plugin) {this.plugin = plugin;}
+    @EventHandler
+    public void Chunkchecking(PlayerChunkLoadEvent event) {
+    }
+}
