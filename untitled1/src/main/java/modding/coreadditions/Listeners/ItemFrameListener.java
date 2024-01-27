e077ed8f48a9336d8604df4e722595bdfc2d20da
ItzFezy <148616668+ItzFezy@users.noreply.github.com>
2024-01-27 15:02:49 -0600

    Update ItemFrameListener.java

--- untitled1/src/main/java/modding/coreadditions/Listeners/ItemFrameListener.java
+++ untitled1/src/main/java/modding/coreadditions/Listeners/ItemFrameListener.java
@@ -1,1 +1,20 @@
+package modding.coreadditions.Listeners;
 
+import io.papermc.paper.event.player.PlayerItemFrameChangeEvent;
+import modding.coreadditions.Main;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class ItemFramelistener implements Listener {
+    private final Main plugin;
+    public ItemFramelistener(Main plugin) {this.plugin = plugin;}
+    @EventHandler
+    public void Item(PlayerItemFrameChangeEvent event) {
+        Material glass = event.getItemStack().getType();
+        if (glass == Material.GLASS) {
+            event.getItemFrame().setVisible(false);
+            event.getItemStack().setType(Material.AIR);
+        }
+    }
+}
